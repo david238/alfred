c7f039941764aa621d7dc74b41781a9d264237de
davidtin <davidtin@opdaconsulting.com>
2018-05-18 15:14:04 -0400

    Initial Commit

---
+++ backend/routes/users.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+const {mongoose} = require('../database/mongoose');
+const {User} = require('../models/user');
+
+
+// POST Users
+router.post('/', function(req, res, next) {
+
+  var user = new User ({
+    name: req.body.name,
+    email: req.body.email
+  });
+
+  user.save().then( (newuser) => {
+    res.send(newuser);
+  }, (error) => {
+    res.status(400).send(error);
+  })
+
+})
+
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  // res.send('respond with a resource');
+
+  User.find().then((user)=>{
+    res.send(user);
+  }, (error) => {
+      res.status(400).send(err);
+  });
+
+});
+
+module.exports = router;
